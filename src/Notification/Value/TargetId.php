37828714819be710bbd77393425eb4dede434a73
Oskar Stark <oskarstark@googlemail.com>
2023-09-25 14:16:07 +0200

    -

--- src/Notification/Value/TargetId.php
+++ src/Notification/Value/TargetId.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/**
+ * This file is part of datana-gmbh/mandantencockpit-contracts.
+ *
+ * (c) Datana GmbH <info@datana.rocks>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Datana\Mandantencockpit\Contracts\Notification\Value;
 
 use Datana\Mandantencockpit\Contracts\Bridge\ApiPlatform\IriUsableInterface;
